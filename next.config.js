db21c2b4cdbea0305cf56afce0e524c827bbf6d8
Fareeha Salahuddin <fareeha@Fareehas-MacBook-Air.local>
2026-01-04 18:40:38 -0700

    Set assetPrefix for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/era-lexicon' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/era-lexicon' : '',
   images: {
     unoptimized: true,
   },
