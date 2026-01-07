c0d55b16f6c337773df6293255f11a9b1aaa4b12
Fareeha Salahuddin <fareeha.salahuddin@gmail.com>
2026-01-06 23:42:26 -0700

    Remove ALL fake tweets - only include real tweets from actual verified accounts

--- app/data/tweets.ts
+++ app/data/tweets.ts
@@ -8,29 +8,7 @@ export interface TweetItem {
 }
 
 export const ERA_TWEETS: Record<string, TweetItem[]> = {
-  '2000-2003': [
-    {
-      text: 'All your base are belong to us',
-      author: 'Zero Wing',
-      handle: '@zerowing',
-      date: '2000',
-      context: 'Translation fail'
-    },
-    {
-      text: 'You can dance if you want to',
-      author: 'Hamster Dance',
-      handle: '@hamsterdance',
-      date: '1998-2003',
-      context: 'Early viral site'
-    },
-    {
-      text: 'Strong Bad Email: DELETED!',
-      author: 'Homestar Runner',
-      handle: '@strongbad',
-      date: '2001',
-      context: 'Flash era'
-    },
-  ],
+  '2000-2003': [],
   
   '2004-2006': [
     {
@@ -39,20 +17,6 @@ export const ERA_TWEETS: Record<string, TweetItem[]> = {
       handle: '@jack',
       date: 'Mar 21, 2006',
       context: 'First tweet ever'
-    },
-    {
-      text: 'LEEEEEEEROOOOY JENKINS',
-      author: 'Leeroy Jenkins',
-      handle: '@leeroy',
-      date: '2005',
-      context: 'WoW legend'
-    },
-    {
-      text: 'O RLY? YA RLY. NO WAI!',
-      author: '4chan',
-      handle: '@4chan',
-      date: '2005',
-      context: 'Owl meme'
     },
   ],
   
@@ -64,40 +28,11 @@ export const ERA_TWEETS: Record<string, TweetItem[]> = {
       date: 'Mar 21, 2006',
       context: 'First tweet ever'
     },
-    {
-      text: 'fail whale',
-      author: 'Twitter Users',
-      handle: '@twitter',
-      date: '2008',
-      context: 'Twitter downtime era'
-    },
   ],
   
-  '2010-2012': [
-    {
-      text: 'The iPhone is not going to get any significant market share',
-      author: 'Steve Ballmer',
-      handle: '@steveb',
-      date: '2007',
-      context: 'Famous prediction fail'
-    },
-    {
-      text: 'We\'re making a dent in the universe',
-      author: 'Steve Jobs',
-      handle: '@stevejobs',
-      date: '2011',
-      context: 'Apple era'
-    },
-  ],
+  '2010-2012': [],
   
   '2013-2015': [
-    {
-      text: 'WHAT ARE THOSEEEEE',
-      author: 'Brandon Moore',
-      handle: '@youngbusco',
-      date: 'Jun 2015',
-      context: 'Viral meme origin'
-    },
     {
       text: 'If only Bradley\'s arm was longer. Best photo ever. #oscars',
       author: 'Ellen DeGeneres',
@@ -116,49 +51,21 @@ export const ERA_TWEETS: Record<string, TweetItem[]> = {
   
   '2016-2018': [
     {
-      text: 'Cash me ousside how bout dah',
-      author: 'Danielle Bregoli',
-      handle: '@bhadbhabie',
-      date: '2016',
-      context: 'Dr Phil meme'
-    },
-    {
       text: 'Yanny or Laurel?',
       author: 'Cloe Feldman',
       handle: '@CloeCouture',
       date: 'May 15, 2018',
       context: 'Audio illusion debate'
     },
-    {
-      text: 'nobody:\nabsolutely nobody:\nstill nobody:\nme:',
-      author: 'Various',
-      handle: '@internet',
-      date: '2018',
-      context: 'Nobody meme format'
-    },
   ],
   
   '2019-2021': [
-    {
-      text: 'Not me having to clean up my Twitter from saying "if I get corona I get corona"',
-      author: 'Brady Sluder',
-      handle: '@BradySluder',
-      date: 'Mar 2020',
-      context: 'COVID denial meme'
-    },
     {
       text: 'i am once again asking for your financial support',
       author: 'Bernie Sanders',
       handle: '@BernieSanders',
       date: 'Jan 2020',
       context: 'Bernie meme'
-    },
-    {
-      text: 'this you?',
-      author: 'Twitter',
-      handle: '@twitter',
-      date: '2020',
-      context: 'Calling out hypocrisy'
     },
   ],
   
@@ -170,44 +77,8 @@ export const ERA_TWEETS: Record<string, TweetItem[]> = {
       date: 'Aug 2024',
       context: 'Demure trend'
     },
-    {
-      text: 'hawk tuah spit on that thang',
-      author: 'Haliey Welch',
-      handle: '@hay_welch',
-      date: 'Jun 2024',
-      context: 'Hawk tuah girl'
-    },
-    {
-      text: 'It\'s corn! A big lump with knobs',
-      author: 'Tariq',
-      handle: '@tariq',
-      date: 'Aug 2022',
-      context: 'Corn kid'
-    },
   ],
   
-  '2025-2026': [
-    {
-      text: 'Founder mode is real and it\'s about being in the details',
-      author: 'Brian Chesky',
-      handle: '@bchesky',
-      date: 'Sep 2024',
-      context: 'Founder mode'
-    },
-    {
-      text: 'we are so back',
-      author: 'Tech Twitter',
-      handle: '@techtwitter',
-      date: '2024-2025',
-      context: 'Optimism posting'
-    },
-    {
-      text: 'brat summer is never over',
-      author: 'Charli XCX',
-      handle: '@charli_xcx',
-      date: 'Jun 2024',
-      context: 'Brat album era'
-    },
-  ],
+  '2025-2026': [],
 }
 
